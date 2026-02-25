cdcf1aaf173f0e3cb74e1ea96eb1d8350d7f411b
Snehal_Patare <89654540+SnelPatare@users.noreply.github.com>
2026-02-24 19:22:31 -0800

    Time: 5 ms (31.64%) | Memory: 50.7 MB (99.74%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,29 +1,41 @@
 class Solution {
     private static final int[][] directions = {{1,0},{-1,0},{0,1},{0,-1}};
     public int numIslands(char[][] grid) {
-        int rows = grid.length, cols = grid[0].length;
+        int row = grid.length;
+        int col = grid[0].length;
+        int island = 0;
 
-        int island =0;
-        for(int r = 0; r<rows; r++){
-            for (int c = 0; c<cols;c++){
+        for(int r = 0; r<row; r++){
+            for (int c = 0; c< col; c++){
                 if(grid[r][c] == '1'){
-                    dfs(grid,r,c);
+                    bfs(grid,r,c);
                     island++;
                 }
             }
-
         }
         return island;
     }
 
-        private void dfs(char[][] grid, int r, int c){
-            if (r<0 || c<0|| r>= grid.length || c>=grid[0].length|| grid[r][c] == '0')
-                return;
+        private void bfs(char[][] grid, int r, int c){
+            Queue<int[]> q = new LinkedList<>();
             grid[r][c] = '0';
-            for (int[] dir:directions){
-                dfs(grid, r+dir[0], c+dir[1]);
-            }
-        }
+            q.add(new int[]{r,c});
 
+            while(!q.isEmpty()){
+                int[] node = q.poll();
+                int row = node[0], col = node[1];
+
+                for(int[] dir:directions){
+                    int nr = row + dir[0];
+                    int nc = col + dir[1];
+                    if(nr >= 0 && nc >= 0 && nr < grid.length && nc < grid[0].length && grid[nr][nc] == '1' ){
+                        q.add(new int[]{nr,nc});
+                        grid[nr][nc] = '0';
+                    }
+                }
+            }
         
-    }
+        }
+        
+    
+}
