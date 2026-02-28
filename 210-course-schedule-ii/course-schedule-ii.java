77c96fde38d53337fa7b4c0d92f07aa9b9835ee2
Snehal_Patare <89654540+SnelPatare@users.noreply.github.com>
2026-02-27 21:17:15 -0800

    Time: 5 ms (82.20%) | Memory: 47 MB (55.99%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,9 +1,7 @@
 class Solution {
-    List<List<Integer>> adjlist;
-    
+    List<List<Integer>> adjlist = new ArrayList<>();
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        adjlist = new ArrayList<>();
-        for(int i=0;i<numCourses;i++){
+        for(int i =0; i<numCourses ; i++){
             adjlist.add(new ArrayList<>());
         }
         int[] indegree = new int[numCourses];
@@ -12,24 +10,26 @@ class Solution {
             adjlist.get(p[1]).add(p[0]);
             indegree[p[0]]++;
         }
+
         Queue<Integer> q = new LinkedList<>();
-        for(int i=0; i<numCourses;i++){
+        int[] result = new int[numCourses];
+        for(int i=0; i<numCourses; i++){
             if(indegree[i] == 0) q.offer(i);
         }
 
-        int[] result = new int[numCourses];
         int index = 0;
-
         while(!q.isEmpty()){
             int cur = q.poll();
             result[index++] = cur;
-            for(int next: adjlist.get(cur)){
+
+            for(int next:adjlist.get(cur)){
                 if(--indegree[next] == 0){
                     q.offer(next);
                 }
             }
         }
-        return index == numCourses?result:new int[0];
+        return index == numCourses ? result : new int[0];
+
         
     }
 }
