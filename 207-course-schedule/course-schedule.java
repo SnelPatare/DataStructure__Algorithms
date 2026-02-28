d517f48b808656262f7f41b916e1b11148aafd0d
Snehal_Patare <89654540+SnelPatare@users.noreply.github.com>
2026-02-27 19:22:47 -0800

    Time: 3 ms (99.78%) | Memory: 47.2 MB (65.81%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,39 +1,36 @@
 class Solution {
-    private List<List<Integer>> adjlist;
-    private int[] state;
+    int[] state;
+    List<List<Integer>> adjlist;
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         adjlist = new ArrayList<>();
-        for(int i =0; i<numCourses; i++){
+        for(int i=0; i<numCourses; i++){
             adjlist.add(new ArrayList<>());
         }
-        //printGraph(adjlist);
-        //return true;
+
         for(int[] p:prerequisites){
             adjlist.get(p[1]).add(p[0]);
         }
-
         state = new int[numCourses];
-        for(int i =0; i<numCourses;i++){
+
+        for(int i=0; i<numCourses; i++){
             if(state[i] == 0){
-                if(dfs(i)){
-                    return false;
-                }
+                if(dfs(i)) return false;
             }
         }
-            
-        return true;
+
+    return true;
+        
     }
-    
 
-        private boolean dfs(int course){
-            if(state[course] == 1) return true;
-            if(state[course] == 2) return false;
+    private boolean dfs(int course){
+        if(state[course] == 1) return true;
+        if(state[course] == 2) return false;
 
-            state[course] =1;
-            for(int i:adjlist.get(course)){
-                if(dfs(i)){return true;}
-            }
-            state[course] = 2;
-            return false;
-        }    
+        state[course] = 1;
+        for(int i : adjlist.get(course)){
+            if(dfs(i)) return true;
+        }
+        state[course] = 2;
+        return false;
+    }
 }
